d590863d630fb277498b25838a6ac5cc471c1955
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2022-12-25 21:47:28 +0500

    HW7

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -12,14 +12,16 @@ import s from './HW6.module.css'
  */
 
 const HW6 = () => {
-    const [value, setValue] = useState<string>('')
+    let [value, setValue] = useState<string>('')
 
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
 
+        // делают студенты
+        value = restoreState<string>('hw6-editable-span-value', '')
+        setValue(value)
     }
 
     return (
