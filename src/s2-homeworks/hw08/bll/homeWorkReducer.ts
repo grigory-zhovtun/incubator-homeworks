34612e807a34ead062bda96882f02760944257ee
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2023-02-04 20:40:34 +0500

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let sortedState: UserType[];
+            if (action.payload === 'up') {
+                sortedState = state.sort((a: UserType, b: UserType) => {
+                    if (a.name > b.name) return 1;
+                    if (a.name < b.name) return -1;
+                    return 0;
+                });
+            } else {
+                sortedState = state.sort((a: UserType, b: UserType) => {
+                    if (a.name < b.name) return 1;
+                    if (a.name > b.name) return -1;
+                    return 0;
+                });
+            }
+            return sortedState;
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((user) => user.age >= action.payload);
         }
         default:
-            return state
+            return state;
     }
 }
